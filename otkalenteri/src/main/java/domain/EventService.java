4a0a1efea9c36f4161ef9b596cf8eec8b470b5b1
014589012 <kallekailamaki@hotmail.com>
2019-04-05 13:28:48 +0300

    update main and eventservice

--- otkalenteri/src/main/java/domain/EventService.java
+++ otkalenteri/src/main/java/domain/EventService.java
@@ -1,5 +1,47 @@
 package domain;
 
+import dao.FileEventDao;
+import dao.FileUserDao;
+import java.util.List;
+
 public class EventService {
+
+    public static boolean login(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    private FileEventDao eventDao;
+    private FileUserDao userDao;
+
+    public EventService(FileEventDao eventDao, FileUserDao userDao) {
+        this.eventDao = eventDao;
+        this.userDao = userDao;
+    }
+
+    public Object getLoggedUser() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void createEvent(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void logout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean createUser(String username, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void markDone(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Event> getUpcoming() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
     
 }
